df6a166dfa5351820176ccd82f5047e725188c4e
LuiosD <luisdartayet@gmail.com>
2023-07-31 22:59:48 -0300

    creates server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Se prendió en el puerto ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
