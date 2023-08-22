7f00e6374aab6dbf8bf577847a2eec0973f7d1e7
LuiosD <luisdartayet@gmail.com>
2023-08-21 21:20:42 -0300

    changes port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Se prendió en el puerto ${PORT}`);
 });
